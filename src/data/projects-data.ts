b52a61835555ee40affe684d3a1c489d3646cbb1
Adnan <adnan.moeen@geekybugs.com>
2025-08-22 11:07:54 +0500

    new fixes

--- src/data/projects-data.ts
+++ src/data/projects-data.ts
@@ -29,22 +29,57 @@ export const projectData: Project[] = [
   tech: 'Flutter (Getx-State Management)',
   description: "Zigu is a driver-focused app that enables load creation and complete trip tracking. Drivers can manage their profile, view employment history, work independently or under a company, and access trip reports with CV download options. The app also allows company-assigned loads, self-created loads, and offers integrated health insurance purchasing for drivers.",
   googlePlayLink: 'https://play.google.com/store/apps/details?id=com.tiecodes.zigu_v2&pcampaignid=web_share',
-  thumbnail: ''
+  thumbnail: '/svg/zigu.svg'
 },
 {
   title: 'Bridge Expedite',
   tech: 'Flutter (Getx-State Management)',
-  description: "",
+  description: "Bridge Expedite is a driver-focused logistics app that connects drivers with transporters through a smart bidding system. Drivers can search and bid on available jobs, while transporters can accept or counter bids from their dashboard. Once a bid is confirmed, the load is assigned, and drivers can start the job with in-app navigation that guides them from pickup to drop-off.",
   googlePlayLink: 'https://play.google.com/store/apps/details?id=tiecodes.com.bridge_expedite&pcampaignid=web_share',
-  thumbnail: ''
+  thumbnail: '/png/bridge_expedite.png'
 },
 
 {
   title: 'Utrack',
   tech: 'Flutter (Getx-State Management)',
-  description: "",
+  description: "Utrack is a smart tracking app that allows users to register, add their devices, and subscribe to a plan for real-time GPS tracking. Once set up, users can view their devices’ live locations on the map, add multiple devices such as family members, and create geofences to get alerts when a device enters or exits specific areas. The app also enables setting custom alerts for each device and viewing notifications from all connected devices within the account.",
   googlePlayLink: 'https://play.google.com/store/apps/details?id=com.tiecodes.u_track&pcampaignid=web_share',
-  thumbnail: ''
+  thumbnail: '/svg/utrack.svg'
+},
+{
+  title: 'Nutri Tech',
+  tech: 'Flutter (Getx-State Management)',
+  description: "Nutri Tech is a business management app that allows users to create a company, add representatives, and manage deals efficiently. Users can create deals, assign tasks either to themselves or other company representatives, and track progress in real time. The app also enables following users, managing profiles, and monitoring everything through a centralized dashboard for seamless business operations.",
+  googlePlayLink: '',
+  thumbnail: '/png/nutriTech.png'
+},
+{
+  title: 'Jewels Airport',
+  tech: 'Flutter (Getx-State Management)',
+  description: "Airport Jewels is a driver-centric logistics app where drivers register with a company and get approved before accessing jobs. Once approved, drivers can view available loads, place bids, and start jobs after bid acceptance. During active jobs, the app also provides in-app chat with customers for smooth communication and coordination.",
+  googlePlayLink: '',
+  thumbnail: '/png/jewelslogo.png'
+},
+{
+  title: 'Taxi2Cardiff',
+  tech: 'Flutter (Bloc-State Management)',
+  description: "Taxi2Cardif is a ride-hailing app built on a single codebase for both customers and drivers. Customers can create ride requests, while drivers view and accept jobs in real time. Once a ride is accepted, drivers complete the trip with integrated navigation and in-app communication.",
+  googlePlayLink: 'Airport Jewels is a driver-centric logistics app where drivers register with a company and get approved before accessing jobs. Once approved, drivers can view available loads, place bids, and start jobs after bid acceptance. During active jobs, the app also provides in-app chat with customers for smooth communication and coordination.',
+  thumbnail: '/png/taxi2Cardiff.png'
+},
+{
+  title: 'NowBy',
+  tech: 'Flutter (Getx-State Management)',
+  description: "NowBy is a location-based social networking app that lets users connect through posts, comments, and interactions near their area. After registering and logging in, users can explore a map-based feed of nearby posts, like and comment, tag friends, and reply to tagged comments. The app also enables sending invitations directly from the contact directory and notifies users when someone from their contacts joins the platform.",
+  googlePlayLink: '',
+  thumbnail: '/png/nowBy.jpg'
+},
+{
+  title: 'close By Jobs',
+  tech: 'Flutter (Getx-State Management)',
+  description: "Close By Job is a location-based job app that uses smart alerts to ensure tasks are completed at the pickup point without delays.",
+  googlePlayLink: 'https://play.google.com/store/apps/details?id=com.tiecodes.rent_ready_homes&pcampaignid=web_share',
+  thumbnail: '/png/closeByJob.jpeg'
 },
 {
   title: 'SwipeLover - The Dating App',
